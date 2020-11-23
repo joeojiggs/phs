cceb48e6de344152fc87040f69ef85c6caf6906a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 00:51:37 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -289,7 +289,7 @@ $num_of_sub = mysqli_num_rows($query);
         <br>
 
         <div>
-            <p><b>REMARK: </b><u> <?php if ($avg > 50){ echo "PASSED"; } else { echo "FAILED" }; ?></u> </p>
+            <p><b>REMARK: </b><u> <?php if ($avg > 50){ echo "PASSED"; } else { echo "FAILED"; }; ?></u> </p>
         </div>
 
         <div>
