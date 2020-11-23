7fbdd0548179268cfdb8711d6a7fef2e49e8e2e5
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 00:44:58 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -111,7 +111,7 @@ $num_of_sub = mysqli_num_rows($query);
                         
                     <div class="col-lg-6">
                         <p>
-                            Class: <u>&nbsp<?php echo $class_name?>&nbsp</u> Academic Session: <?php $qurey = mysqli_query($con, "select * from admin");
+                            Class: <u>&nbsp<?php echo $class_name?>&nbsp</u> Academic Session: <u>&nbsp<?php $qurey = mysqli_query($con, "select * from admin");
             $row = mysqli_fetch_assoc($qurey);
             echo ($row['session']);
                 ?> Term:
