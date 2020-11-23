b7b9837e844b59bafc9b77d1f3b37162a687eb59
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 00:33:13 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -6,7 +6,7 @@ include('php-includes/check-login-admin.php');
 
 if (isset($_POST['ses_submit'])){
      
-    $first_name = mysqli_real_escape_string($con,$_POST['session']);
+    $session = mysqli_real_escape_string($con,$_POST['session']);
 
 ?>
 <!DOCTYPE html>
