625607771d611d07a2c7fdc280e78e9144ed778f
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-23 19:24:13 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -67,7 +67,7 @@ include('php-includes/check-login-admin.php');
 
                     <!-- Button trigger modal -->
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                    Launch demo modal
+                    
                     </button>
 
                     <!-- Modal -->
