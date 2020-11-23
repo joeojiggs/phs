f3e41347d97994ad7099f207f4a66e006776b1b4
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 00:36:15 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -10,8 +10,10 @@ if (isset($_POST['ses_submit'])){
 
     $query = mysqli_query($con, "update set admin `session` = '$session'");
 
-    if (empty($query))
-    
+    if (empty($query)){
+        
+    }
+
 
 ?>
 <!DOCTYPE html>
