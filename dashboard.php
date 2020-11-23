99e7d0b516d50cd65c03f133cc6649cf00a292d6
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-23 19:16:50 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -58,7 +58,7 @@ include('php-includes/check-login-admin.php');
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
-            <?php $qurey = mysqli_query($con, "select * from admin")
+            <?php $qurey = mysqli_query($con, "select * from admin");
             $row = mysqli_fetch_assoc($qurey)
             ?>
             <!-- /.row -->
