36f9412a08708f6384209e377a8ee073ee57cd1b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-23 19:21:01 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -63,7 +63,7 @@ include('php-includes/check-login-admin.php');
             if (empty($row['session'])){
                 ?>
                 <div class="alert alert-danger" role="alert">
-                    Academic Session has not be set. Please Input a
+                    Academic Session has not be set. Please Input the Acade
                 </div>
                 <?php
             }
