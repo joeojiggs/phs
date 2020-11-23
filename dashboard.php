f0419e2031cb2ddcc278aab743293311d86f4eb2
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-23 19:14:49 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -58,7 +58,7 @@ include('php-includes/check-login-admin.php');
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
-            <?php mysq?>
+            <?php mysqli_?>
             <!-- /.row -->
             <div class="row">
                 <div class="col-lg-3 col-md-6">
