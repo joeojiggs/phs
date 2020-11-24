f1d296033f75be97bb743bee8f0b56ccbc27dcb6
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 12:04:29 +0100

    Gitacora saved 2 changed files

--- annual/result.php
+++ annual/result.php
@@ -385,7 +385,7 @@ $num_of_sub = mysqli_num_rows($query);
         </div>
         <div>
         <?php if ($class_name == "kg1" || $class_name == "kg2" || $class_name == "kg3" || $class_name == "prenur" ) {
-                            ?> <img src="../images/pcsstamp." width ="100%"> <?php ;
+                            ?> <img src="../images/pcsstamp.png" width ="100%"> <?php ;
                         }elseif ($class_name == "basic1" || $class_name == "basic2" || $class_name == "basic3" || $class_name == "basic4" || $class_name == "basic5" ){
                             ?><img src="../images/pcs.jpg" width ="100%"><?php ;
                         }elseif ( $class_name == "jss1" || $class_name == "jss2" || $class_name == "jss3") {
