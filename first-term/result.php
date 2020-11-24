6580d2962efd932bd4b4086c4df5f711650e27cf
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 02:56:25 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -363,7 +363,7 @@ $num_of_sub = mysqli_num_rows($query);
         <div>
             <p>School Fees: &nbsp &nbsp <?php $qurey = mysqli_query($con, "select * from admin");
             $row = mysqli_fetch_assoc($qurey);
-            if($class_name == "kg1" || $class_name == "kg2" || $class_name == "kg3" || $class_name == kg1 || ){
+            if($class_name == "kg1" || $class_name == "kg2" || $class_name == "kg3" || $class_name == "" || ){
                 echo ($row['session']);
             }elseif($class_name ==){
                 echo ($row['session']);
