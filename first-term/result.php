12d1e145090cbec4efbe6c5bec6b8d29f6014b49
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 01:38:29 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -247,7 +247,7 @@ $num_of_sub = mysqli_num_rows($query);
                         
                         <td class= "text-primary"><b>
                         <?php echo $grade ;
-                            if  ($avg == "A"){
+                            if  ($avg > 90){
                                 echo "&nbsp &nbsp(Distinction)";   
                             //}elseif  ($avg > 80 ){
                                // echo "&nbsp &nbsp(Excellent)";   
