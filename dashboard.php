9683e83380d37e69b9803e4bf2f5c4546fee55ba
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 02:44:33 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -130,7 +130,7 @@ if (isset($_POST['ses_submit'])){
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Enter Current Academic Session</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Enter School Fees Comment</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
