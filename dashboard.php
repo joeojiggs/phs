a3e442175532c65a75f5448f810fee955dac6f9a
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 02:36:54 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -115,7 +115,7 @@ if (isset($_POST['ses_submit'])){
                 <?php
             }
 
-            if (empty($row['kg']) || empty($row['basic'])){
+            if (empty($row['kg']) || empty($row['basic']) ){
                 ?>
                 <div class="alert alert-danger" role="alert">
                     Academic Session has not be set. Please Input the Academic Session!
