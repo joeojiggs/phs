c99bfbf872fb11af9a6da4c1f98d9a3eaec7047c
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 01:55:06 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -144,7 +144,7 @@ $num_of_sub = mysqli_num_rows($query);
           <th>Subjects</th>
           <th>1st Test(20%)</th>
           <th>  
-            2nd Test()
+            2nd Test()20%
           </th>
           <th>
             Exam
