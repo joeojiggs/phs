7024241c846ff109567529600f493d85caa641f2
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 02:40:28 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -139,7 +139,7 @@ if (isset($_POST['ses_submit'])){
                             <form role="form" method="post">
                                        
                                 <div class="form-group">
-                                    <label>PRE NURSERY AND K</label>
+                                    <label>PRE NURSERY AND K.G</label>
                                     <input class="form-control" placeholder="2019/2020" name="session" REQUIRED>
                                 </div>
 
