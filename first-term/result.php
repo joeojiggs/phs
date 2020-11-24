6c6e9db69ba62445b8f3dc410e1eaabda5ddd819
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 02:58:00 +0100

    Gitacora saved 1 changed files

--- first-term/result.php
+++ first-term/result.php
@@ -369,7 +369,7 @@ $num_of_sub = mysqli_num_rows($query);
                 echo ($row['session']);
             }elseif( $class_name == "jss1"){
                 echo ($row['session']);
-            }elseif($class_name ==){
+            }elseif($class_name == ""){
                 echo ($row['session']);
             }
              ?>  </p>
