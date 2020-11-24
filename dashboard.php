afe1e9c91a8448169b1a17b0dd2c6c6447a4855b
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-24 02:41:22 +0100

    Gitacora saved 1 changed files

--- dashboard.php
+++ dashboard.php
@@ -154,7 +154,7 @@ if (isset($_POST['ses_submit'])){
                                 </div>
 
                                 <div class="form-group">
-                                    <label>Academic Session</label>
+                                    <label>SSS 1-</label>
                                     <input class="form-control" placeholder="2019/2020" name="session" REQUIRED>
                                 </div>
                         </div>
