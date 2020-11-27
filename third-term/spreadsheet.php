99175897e3d0e51e49840890379c8c90544c341d
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-27 14:03:10 +0100

    Gitacora saved 1 changed files

--- third-term/spreadsheet.php
+++ third-term/spreadsheet.php
@@ -87,7 +87,6 @@ if (isset($_POST['process'])){
 
                 $avg = round ($avg, 2);
 
-
                  $sql4 = "update students set `third_total` = '$grand_total', `third_avg` = '$avg' where Reg_Num = '$reg_no' and Class='$class_name'";
                  $query4 = mysqli_query ($con, $sql4);
                         
