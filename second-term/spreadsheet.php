f205cd525b41f12f537d884c8c310fc61237d4c7
JOSEPH OJIGHORO <40868031+fabulouz004@users.noreply.github.com>
2020-11-27 14:02:51 +0100

    Gitacora saved 1 changed files

--- second-term/spreadsheet.php
+++ second-term/spreadsheet.php
@@ -85,7 +85,8 @@ if (isset($_POST['process'])){
 
                 $avg = $grand_total/$num_of_sub;
 
-                
+                $avg = round ($avg, 2);
+
 
                  $sql4 = "update students set `second_total` = '$grand_total', `second_avg` = '$avg' where Reg_Num = '$reg_no' and Class='$class_name'";
                  $query4 = mysqli_query ($con, $sql4);
